d6e8e2923bb2cf312ee7a123d9dc68ca85480e6d
五十嵐博司 <aaa@test.com>
2016-05-27 17:49:13 +0900

    first commit

---
+++ SampleMailVB/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Net
+Imports System.Net.Mail
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+            Dim msg As New MailMessage("igarashi@hnps.co.jp", "igarashi@hnps.co.jp", "テスト", "これはテストメールです。")
+        Dim SmtpClientObj As New SmtpClient()
+        SmtpClientObj.Host = "smtp.hnps.co.jp"
+        SmtpClientObj.Port = 587
+        SmtpClientObj.DeliveryMethod = SmtpDeliveryMethod.Network
+
+        Dim NTC As New System.Net.NetworkCredential()
+
+        SmtpClientObj.Credentials = New NetworkCredential("igarashi@hnps.co.jp", "hiGara123")
+
+
+
+        'メッセージを送信する
+        SmtpClientObj.Send(msg)
+        msg.Dispose()
+        SmtpClientObj.Dispose()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+
+    End Sub
+End Class
